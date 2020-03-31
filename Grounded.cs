62a4adafd9a416062b16e46bbd946fadf2fde6ab
Doruzzz <44002080+Doruzzz@users.noreply.github.com>
2020-03-31 12:58:52 +0200

    Scripts upload

---
+++ Grounded.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+    void Start()
+    {
+        Player = gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<playercontroller>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<playercontroller>().isGrounded = false;
+        }
+    }
+}
---
+++ playercontroller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    private Rigidbody rigidbody;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public bool isGrounded;
+    public float jumpVelocity;
+
+    private void Awake()
+    {
+        rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, movevertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rigidbody.velocity = Vector3.up * jumpVelocity;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
